d3c3ae08cf7740993f945d3e368cbee85fb2ff28
eeeun <67456159+eeeunpark@users.noreply.github.com>
2020-08-02 23:25:45 +0900

    Update homework-03-짭짤.py

--- homework/homework-03-짭짤.py
+++ homework/homework-03-짭짤.py
@@ -12,7 +12,7 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
-#body-content > div.newest-list > div > table > tbody > tr:nth-child(1)
+
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
@@ -29,4 +29,3 @@ for tr in trs:
     }
 
     db.genie.insert_one(doc)
-    
